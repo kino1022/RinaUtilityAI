ba4a536721ab564dbbba7a53c3c12d9b5a9a0525
kino1022 <132138562+kino1022@users.noreply.github.com>
2026-05-23 14:21:36 +0900

    update machine

--- Runtime/APrioritizationMachine.cs
+++ Runtime/APrioritizationMachine.cs
@@ -111,7 +111,5 @@ namespace RinaUtilityAI {
 
 			}
 		}
-
-
 	}
 }
--- Runtime/RinaUtilityLocalInstaller.cs
+++ Runtime/RinaUtilityLocalInstaller.cs
@@ -1,4 +1,6 @@
+using RinaUtilityAI.Behaviour;
 using Sirenix.OdinInspector;
+using Sirenix.Serialization;
 using UnityEngine.Assertions;
 using VContainer;
 using VContainer.Unity;
@@ -6,14 +8,25 @@ using VContainer.Unity;
 namespace RinaUtilityAI {
 	public class RinaUtilityLocalInstaller : SerializedMonoBehaviour, IInstaller {
 
+		[OdinSerialize]
+		private IDefaultBehaviourHolder _defaultBehaviour;
+
 		public void Install(IContainerBuilder builder) {
 			Assert.IsNotNull(builder);
+
 			builder
 				.RegisterFactory<IObjectResolver, UtilityOwnerReference>(resolver => {
 					var ownerRef = new UtilityOwnerReference(gameObject, resolver);
 					Assert.IsNotNull(ownerRef);
 					return ownerRef;
 				});
+
+			_defaultBehaviour ??= gameObject.GetComponentInChildren<IDefaultBehaviourHolder>();
+			Assert.IsNotNull(_defaultBehaviour);
+			builder
+				.RegisterInstance(_defaultBehaviour)
+				.As<IUtilityBehaviourInstance>();
+			
 		}
 	}
 }
